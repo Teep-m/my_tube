642f035ae665d4446beca96ce2eafb5d6b1f6f24
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-09-10 22:52:16 +0900

    'almost'

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  url: BASE_URL,
   params: {
-    maxResults: '10'
+    // 試運転中はリクエスト個数を5にしております。
+    maxResults: '5' 
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
 };
